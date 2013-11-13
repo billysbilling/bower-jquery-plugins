fbf60d7f06c92e838f9d55bd0c2a4c11be5101f3
Sebastian Seilund <sebastian@seilund.com>
2013-11-13 15:19:55 -0800

    enter event should only fire once per fresh (i.e. not repeat when holding it down)

--- src/custom/enter.js
+++ src/custom/enter.js
@@ -1,7 +1,16 @@
 jQuery.fn.enter = function (callback) {
+    var isDown = false;
+    
     this.keypress(function(e) {
-        if (e.which == jQuery.keyCode.ENTER) {
+        if (!isDown && e.which === jQuery.keyCode.ENTER) {
+            isDown = true;
             callback(e);
+        }
+    });
+    
+    this.keyup(function(e) {
+        if (e.which === jQuery.keyCode.ENTER) {
+            isDown = false;
         }
     });
     return this;
